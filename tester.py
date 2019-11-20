02c584c7ed71f9ed398ef336ff8566c30153bb4f
kailichou <35256506+kailichou@users.noreply.github.com>
2019-11-20 16:28:31 +0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -2,18 +2,13 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 import pandas as pd
 
-
-
-        
-   
-
-
-
+#build a spider 
 def spider(self):
+    ##since we are crawling 62 pages and 50 listings roughly, I only used count to simply record crawling process.
     count = 0
-    #insert p values in link
     jobs = []
     
+    #insert p values in link
     urls = [self.format(p) for p in range(1,62)]
     for url in urls:
         webpage = uReq(url).read().decode('gbk').encode('utf-8') 
@@ -30,8 +25,6 @@ def spider(self):
             location = containers.parent.find(class_='t3').text
             salary = containers.parent.find(class_='t4').text
             date = containers.parent.find(class_='t5').text
-        
-            #job dictionaries map job_title to value scrapped 
             
             
             if job_title is not None:  
@@ -44,19 +37,17 @@ def spider(self):
                 #a job just added
                 count += 1
             print("so far it has crawled ", count," jobs." + '\n')
-
-
-  
+    
     spiderjob = pd.DataFrame(jobs)
+    spiderjob.to_csv("spiders")
+    ##have a look at datasets
     return spiderjob.head()
-   
+
+     
 link = ("https://search.51job.com/list/000000,000000,0000,00,9,99,data%2520analyst,2,{}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=")    
 print(spider(link))
 
-  
 
 
-    
-
-    
+###To be continue
   
